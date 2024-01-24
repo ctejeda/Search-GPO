11d3c286818f7b534019bd81bae9be949adf8f0b
Chris Tejeda <ctejeda@tecknicos.com>
2024-01-24 16:06:35 -0500

    Update Search-GPO.ps1

--- Search-GPO.ps1
+++ Search-GPO.ps1
@@ -11,33 +11,36 @@ Function Search-GPO {
     $domain = (Get-ADDomain).DNSRoot
     $gpos = Get-GPO -All -Domain $domain
 
+    $ous = Get-ADOrganizationalUnit -Filter * -Properties gPLink
+
     foreach ($gpo in $gpos) {
         $gpoid = $gpo.Id
         $gpoDisplayName = $gpo.DisplayName
         $report = Get-GPOReport -Guid $gpoid -ReportType Xml -Domain $domain
 
-        $links = Get-GPOLink -Guid $gpoid
-        foreach ($link in $links) {
-            $ou = $link.SOMName
-            $path = $link.SOMPath
+        foreach ($ou in $ous) {
+            if ($ou.gPLink -match $gpoid) {
+                $location = $ou.Name
+                $path = $ou.DistinguishedName
 
-            if ($report -match $string) {
-                $gpoarray += [pscustomobject] @{
-                    GPO = $gpoDisplayName
-                    String = $string
-                    Matched = "True"
-                    GUID = $gpoid
-                    Location = $ou
-                    Path = $path
-                }
-            } else {
-                $gpoarray += [pscustomobject] @{
-                    GPO = $gpoDisplayName
-                    String = $string
-                    Matched = "False"
-                    GUID = $gpoid
-                    Location = $ou
-                    Path = $path
+                if ($report -match $string) {
+                    $gpoarray += [pscustomobject] @{
+                        GPO = $gpoDisplayName
+                        String = $string
+                        Matched = "True"
+                        GUID = $gpoid
+                        Location = $location
+                        Path = $path
+                    }
+                } else {
+                    $gpoarray += [pscustomobject] @{
+                        GPO = $gpoDisplayName
+                        String = $string
+                        Matched = "False"
+                        GUID = $gpoid
+                        Location = $location
+                        Path = $path
+                    }
                 }
             }
         }
