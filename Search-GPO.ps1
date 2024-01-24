64d751987590a36b5b2c5cc9465e4d58181f3e2c
Chris Tejeda <ctejeda@tecknicos.com>
2024-01-24 15:53:58 -0500

    Update Search-GPO.ps1

--- Search-GPO.ps1
+++ Search-GPO.ps1
@@ -8,40 +8,37 @@ Function Search-GPO {
     )
 
     $gpoarray = @()
-    $domain = (Get-CIMInstance CIM_ComputerSystem).Domain.toLower()
-    $site = [void](nltest /dsgetsite)
-    $dc = if ($site) {
-        (Get-ADDomainController -Discover -Domain $domain -SiteName $site).hostname[0].toLower()
-    } else {
-        (Get-ADDomainController -Discover -Domain $domain).hostname[0].toLower()
-    }
+    $domain = (Get-ADDomain).DNSRoot
+    $gpos = Get-GPO -All -Domain $domain
 
-    $DomainGPOs = Get-GPO -All -Domain $domain -Server $dc
-    
-    foreach ($gpo in $DomainGPOs) {
+    foreach ($gpo in $gpos) {
         $gpoid = $gpo.Id
         $gpoDisplayName = $gpo.DisplayName
-        $report = Get-GPOReport -Guid $gpoid -ReportType Xml -Domain $domain -Server $dc
+        $report = Get-GPOReport -Guid $gpoid -ReportType Xml -Domain $domain
 
-        # Find where the GPO is linked
-        $gpoLinks = Get-GPInheritance -Target "ou=$domain,dc=$dc" | Select-Object -ExpandProperty GpoLinks
-        $ouPaths = $gpoLinks | Where-Object { $_.GpoId -eq $gpoid } | ForEach-Object { $_.Target }
+        $links = Get-GPOLink -Guid $gpoid
+        foreach ($link in $links) {
+            $ou = $link.SOMName
+            $path = $link.SOMPath
 
-        if ($report -match $string) {
-            $gpoarray += [pscustomobject] @{
-                GPO = $gpoDisplayName
-                String = $string
-                Matched = "True"
-                GUID = "$gpoid"
-                OUs = $ouPaths -join ", "
-            }
-        } else {
-            $gpoarray += [pscustomobject] @{
-                GPO = $gpoDisplayName
-                String = $string
-                Matched = "False"
-                GUID = "$gpoid"
-                OUs = $ouPaths -join ", "
+            if ($report -match $string) {
+                $gpoarray += [pscustomobject] @{
+                    GPO = $gpoDisplayName
+                    String = $string
+                    Matched = "True"
+                    GUID = $gpoid
+                    Location = $ou
+                    Path = $path
+                }
+            } else {
+                $gpoarray += [pscustomobject] @{
+                    GPO = $gpoDisplayName
+                    String = $string
+                    Matched = "False"
+                    GUID = $gpoid
+                    Location = $ou
+                    Path = $path
+                }
             }
         }
     }
